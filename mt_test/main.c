3309da29e51fd25662e4e4a144705cea7696ac3e
zzvsjs1 <benzzvsjs@gmail.com>
2022-07-06 09:21:57 +1000

    Code cleanup

--- mt_test/main.c
+++ mt_test/main.c
@@ -1,17 +1,21 @@
 #include "thread.h"
 
+int a = 5, b = 6;
+
 void print(int i)
 {
-    while (true)
-    {
-        printf("Hello thread %d!\n", i);
-    }
+    atomic_fetch_add
+    a++;   
 }
 
 int main()
 {
     create(print);
     create(print);
+
+    join();
+
+    printf("%d\n", a);
     
     return 0;
 }
